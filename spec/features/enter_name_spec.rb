cd23de8597c14112a0a68fed0293d6ef113b95ff
Dana Gajewska <mdgajewska@outlook.com>
2018-11-14 13:55:59 +0000

    Update spec and index.erb

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,4 +1,8 @@
 feature 'Names' do
+
+  # As two Players,
+  # So we can play a personalised game of Battle,
+  # We want to Start a fight by entering our names and seeing them
   scenario 'submitting names' do
     visit('/')
     fill_in :player_1_name, with: 'Skeletor'
