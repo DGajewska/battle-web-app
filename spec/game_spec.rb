5ab3859d50d53d3930b5a8a3a931ef40edda3803
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-14 16:35:59 +0000

    adds a test for attackin through the game object

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,4 +9,12 @@ describe Game do
     expect(subject.player_1).to eq(player_instance)
     expect(subject.player_2).to eq(player_instance)
   end
+
+  describe '#attack' do
+    it 'when player 1 attacks player 2 they take damage' do
+      expect(subject.player_2).to receive(:take_damage)
+      subject.attack(player_1)
+    end
+  end
+
 end
